deccb9b35c76b2a6f46c4edc1905e976bd5007ae
Phurin Nararat <phurin.nrt@gmail.com>
2024-09-24 20:46:37 +0700

    Debug commit
    Try dynamic env variable for NEXT_PUBLIC_API_URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://hr-360-one.vercel.app',
+  },
   async redirects() {
       return [
         {
