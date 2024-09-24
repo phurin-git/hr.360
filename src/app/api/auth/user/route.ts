e29fa7bc7490bfd7a79f225d8887d4dead2c69ef
Phurin Nararat <phurin.nrt@gmail.com>
2024-09-24 22:27:48 +0700

    Update commit
    Fix: take bug out from user verify api route and will use api verify for excercise (but need to run on localhost)

--- src/app/api/auth/user/route.ts
+++ src/app/api/auth/user/route.ts
@@ -3,7 +3,17 @@ import argon2 from 'argon2';
 import userDB from '@/app/data/userData';
 
 export async function POST(req: NextRequest) {
-    const { email, password } = await req.json();
-    const user = userDB.find(async (user) => user.email === email && await argon2.verify(user.password, password));
-  return NextResponse.json(user ? user : {});
-}
+  const { email, password } = await req.json();
+
+  // Find user by email
+  const user = userDB.find((u) => u.email === email);
+
+  // If no user is found or password does not match, return empty object
+  if (!user || !(await argon2.verify(user.password, password))) {
+    return NextResponse.json(null);
+  }
+
+  // Return safe user data (excluding password)
+  const { password: _, ...safeUserData } = user;
+  return NextResponse.json(safeUserData);
+};
