5f879ad56fed9c7b4d8f476b63dbfce63711d4ef
Phurin Nararat <phurin.nrt@gmail.com>
2024-09-24 21:33:06 +0700

    Debug commit
    Try to access api endpoint by middleware when deploy with vercel

--- src/app/data/userData.ts
+++ src/app/data/userData.ts
@@ -5,21 +5,21 @@ const userData: User[] = [
     id: "1",
     name: "Super User",
     email: "superuser@account.com",
-    password: "superuser",
+    password: "$argon2id$v=19$m=65536,t=3,p=4$YvYGwCqKYf9d7UwJSSlIVg$QZHtQ7rmXTNTrUWd2BHXDbd7OP7g34tWkbKFXmFJeZs",
     role: "Super User",
   },
 {
   id: "2",
   name: "Admin User",
   email: "admin@account.com",
-  password: "adminuser",
+  password: "$argon2id$v=19$m=65536,t=3,p=4$YvYGwCqKYf9d7UwJSSlIVg$QZHtQ7rmXTNTrUWd2BHXDbd7OP7g34tWkbKFXmFJeZs",
   role: "Admin",
 },
 {
   id: "3",
   name: "Regular User",
   email: "user@account.com",
-  password: "regularuser",
+  password: "$argon2id$v=19$m=65536,t=3,p=4$YvYGwCqKYf9d7UwJSSlIVg$QZHtQ7rmXTNTrUWd2BHXDbd7OP7g34tWkbKFXmFJeZs",
   role: "User",
 }
 ];
